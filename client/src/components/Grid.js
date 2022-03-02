97d436ed63db30ee9e450a8d814d20e2aabb4abb
Dylan Brain <dylanbrain@googlemail.com>
2022-03-02 20:15:52 +0000

    node is cleaned before being passed in

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -54,29 +54,46 @@ class Grid extends Component {
 
   };
 
-  initCarGrid = () => {
-    // initialised grid based off data in state
-    const { constWidgetCar, carOptions } = this.state;
-    const node = {};
+  initCarGrid = (newNode) => {
+          // initialised grid based off data in state
+      const { constWidgetCar, carOptions } = this.state;
+      const node = {};
 
-    // adds grid to the element with the id gridWrapper
-    this.grid = GridStack.addGrid(
-      document.getElementById("carGridWrapper"),
-      carOptions
-    );
-
-    // sets placement based on data in state
-    for (let i = 0; i < constWidgetCar.length; i++) {
-      node.content = String(this.state.constWidgetCar[i].name);
-      node.x = constWidgetCar[i].x;
-      node.y = constWidgetCar[i].y;
-      node.w = constWidgetCar[i].w;
-      node.h = constWidgetCar[i].h;
+      // adds grid to the element with the id gridWrapper
+      this.grid = GridStack.addGrid(
+        document.getElementById("carGridWrapper"),
+        carOptions
+      );
+ 
+    if (newNode) {
+      const cleanNewNode = {};
+      cleanNewNode.content = newNode.content;
+      cleanNewNode.x = newNode.x;
+      cleanNewNode.y = newNode.y;
+      cleanNewNode.w = newNode.w;
+      cleanNewNode.h = newNode.h;
       // options
-      node.noResize = true;
-      node.locked = true;
-      node.noMove = false;
-      this.grid.addWidget(node);
+      cleanNewNode.noResize = true;
+      cleanNewNode.locked = true;
+      cleanNewNode.noMove = false;
+      console.log(cleanNewNode)
+      this.grid.addWidget(cleanNewNode);
+      return;
+    } else {
+
+      // sets placement based on data in state
+      for (let i = 0; i < constWidgetCar.length; i++) {
+        node.content = String(this.state.constWidgetCar[i].name);
+        node.x = constWidgetCar[i].x;
+        node.y = constWidgetCar[i].y;
+        node.w = constWidgetCar[i].w;
+        node.h = constWidgetCar[i].h;
+        // options
+        node.noResize = true;
+        node.locked = true;
+        node.noMove = false;
+        this.grid.addWidget(node);
+      }
     }
 
     this.handleCarGrid(this.grid)
@@ -92,7 +109,9 @@ class Grid extends Component {
 
   handleCarGrid = (grid) => {
     this.grid.on("removed", (e, item) => {
-      this.grid.addWidget(item[0]) // adds removed node back to the car grid
+     //this.grid.addWidget(item[0]) // adds removed node back to the car grid
+      console.log(item)
+      this.initCarGrid(item[0]);
     })
 
   };
